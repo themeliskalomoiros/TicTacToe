123d4de86e23b3bf31a97e70499f2a221406dfef
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-04-05 21:26:47 +0200

    Created tests for Box

---
+++ GameEngine.Test/BoxShould.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------------
+
+using GameEngine;
+using Xunit;
+
+//-----------------------------------------------------------------------------
+
+namespace GameEngine.Test {
+
+//-----------------------------------------------------------------------------
+
+public class BoxShould {
+
+//-----------------------------------------------------------------------------
+
+#region Arrange
+
+//-----------------------------------------------------------------------------
+
+private Box sut = new Box();
+
+//-----------------------------------------------------------------------------
+
+#endregion
+#region Tests
+
+//-----------------------------------------------------------------------------
+
+[Fact]
+public void HaveDefaultMarkingToNone()
+{
+  Assert.Equal(Marking.None, sut.Marking);
+}
+
+//-----------------------------------------------------------------------------
+
+[Fact]
+public void RaisePropertyChanged()
+{
+  Assert.PropertyChanged(
+    sut,
+    nameof(sut.Marking),
+    () => sut.Marking = Marking.Circle);
+}
+
+//-----------------------------------------------------------------------------
+
+[Fact]
+public void NotRaisePropertyChangeWhenSameValueIsSet()
+{
+  int eventRaiseCount = 0;
+  sut.PropertyChanged += (s, e) => ++eventRaiseCount;
+
+  sut.Marking = Marking.Circle;
+  Assert.Equal(1, eventRaiseCount);
+
+  sut.Marking = Marking.Circle;
+  Assert.Equal(1, eventRaiseCount);
+}
+
+// ----------------------------------------------------------------------------
+
+#endregion
+
+// ----------------------------------------------------------------------------
+
+} // <-- end of type body -->
+
+// ----------------------------------------------------------------------------
+
+} // <-- end of namespace body -->
