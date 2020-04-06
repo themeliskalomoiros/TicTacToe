8a8319aac545bcff01df247ee353e1558668380c
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-04-06 22:21:37 +0200

    Created GameState tests

--- GameEngine.Test/GameStateShould.cs
+++ GameEngine.Test/GameStateShould.cs
@@ -1,11 +1,14 @@
 ﻿//-----------------------------------------------------------------------------
 
 using GameEngine;
+using GameEngine.Events;
+using System;
 using Xunit;
 
 //-----------------------------------------------------------------------------
 
-namespace GameEngine.Test {
+namespace GameEngine.Test
+{
 
 //-----------------------------------------------------------------------------
 
@@ -15,6 +18,8 @@ public class GameStateShould {
 
 #region Arrange
 
+private GameState sut = new GameState();
+
 //-----------------------------------------------------------------------------
 
 #endregion
@@ -23,9 +28,128 @@ public class GameStateShould {
 // ----------------------------------------------------------------------------
 
 [Fact]
-public void Start()
+public void StartWithCrossesTurnByDefault()
 {
+  Assert.True(sut.IsCrossesTurn);
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void StartWithCirclesTurnIfConfigured()
+{
+  sut = new GameState(false);
+  Assert.False(sut.IsCrossesTurn);
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void MarkCrossesWhenItsCrossesTurn()
+{
+  sut.Mark(3);
+
+  Assert.Equal(Marking.Cross, sut.GetMark(3));
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void MarkCirclesWhenItsCirclesTurn()
+{
+  // By default crosses plays first
+  sut.Mark(0); 
+  sut.Mark(1);
+
+  Assert.Equal(Marking.Circle, sut.GetMark(1));
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void RaiseCrossesMarkEvent()
+{
+  Assert.Raises<CrossesMarkEventArgs>(
+    handler => sut.CrossesMarkEvent += handler,
+    handler => sut.CrossesMarkEvent += handler,
+    () => sut.Mark(4));
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void RaiseCirclesMarkEvent()
+{
+  Assert.Raises<CirclesMarkEventArgs>(
+    handler => sut.CirclesMarkEvent += handler,
+    handler => sut.CirclesMarkEvent += handler,
+    () => 
+    { 
+      sut.Mark(4);
+      sut.Mark(5);
+    });
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void ReportDraw()
+{
+  Action draw = () => 
+  {
+    sut.Mark(0);
+    sut.Mark(1);
+    sut.Mark(2);
+    sut.Mark(4);
+    sut.Mark(7);
+    sut.Mark(3);
+    sut.Mark(5);
+    sut.Mark(8);
+    sut.Mark(6);
+  };
   
+  sut.CompletionEvent += (s, e) => Assert.Equal(Result.Draw, e.Result); 
+  
+  draw();
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void ReportCrossesWin()
+{
+  Action win = () => 
+  {
+    sut.Mark(4);
+    sut.Mark(1);
+    sut.Mark(2);
+    sut.Mark(3);
+    sut.Mark(7);
+  };
+
+  sut.CompletionEvent += (s, e) => Assert.Equal(Result.Draw, e.Result);
+
+  win();
+}
+
+// ----------------------------------------------------------------------------
+
+[Fact]
+public void ReportCirclesWin()
+{
+  Action win = () => 
+  {
+    sut.Mark(4);
+    sut.Mark(0);
+    sut.Mark(8);
+    sut.Mark(2);
+    sut.Mark(7);
+    sut.Mark(1);
+  };
+
+  sut.CompletionEvent += (s, e) => Assert.Equal(Result.Draw, e.Result);
+
+  win();
 }
 
 // ----------------------------------------------------------------------------
---
+++ GameEngine/GameState.cs
@@ -0,0 +1,191 @@
+﻿//-----------------------------------------------------------------------------
+
+using GameEngine.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+
+//-----------------------------------------------------------------------------
+
+namespace GameEngine {
+
+//-----------------------------------------------------------------------------
+
+public class GameState {
+
+//-----------------------------------------------------------------------------
+
+#region Properties
+
+//-----------------------------------------------------------------------------
+
+public bool IsCrossesTurn { get; private set; } = true;
+
+//-----------------------------------------------------------------------------
+
+#endregion
+#region Public Methods
+
+//-----------------------------------------------------------------------------
+
+public void Mark(
+  int position)
+{
+  ReportMarking(position);
+  ReportResult();
+}
+
+//-----------------------------------------------------------------------------
+
+public Marking GetMark(
+  int position)
+{
+  return board.GetMarking(position);
+}
+
+//-----------------------------------------------------------------------------
+
+#endregion
+#region Constructors
+
+//-----------------------------------------------------------------------------
+
+public GameState(
+  bool isCrossesTurn = true)
+{
+  IsCrossesTurn = isCrossesTurn;
+}
+
+//-----------------------------------------------------------------------------
+
+#endregion
+#region Event Handlers
+
+//-----------------------------------------------------------------------------
+
+public event EventHandler<CrossesMarkEventArgs> CrossesMarkEvent;
+public event EventHandler<CirclesMarkEventArgs> CirclesMarkEvent;
+public event EventHandler<CompletionEventArgs> CompletionEvent;
+
+//-----------------------------------------------------------------------------
+
+#endregion
+#region Private Methods
+
+//-----------------------------------------------------------------------------
+
+private void RaiseCrossesMarkEvent(
+  int position)
+{
+  CrossesMarkEvent?.Invoke(this, new CrossesMarkEventArgs(position));
+}
+
+//-----------------------------------------------------------------------------
+
+private void RaiseCirclesMarkEvent(
+  int position)
+{
+  CirclesMarkEvent?.Invoke(this, new CirclesMarkEventArgs(position));
+}
+
+//-----------------------------------------------------------------------------
+
+private void RaiseCompletionEvent(Result result)
+{
+  CompletionEvent?.Invoke(this, new CompletionEventArgs(result));
+}
+
+//-----------------------------------------------------------------------------
+
+private void ReportResult()
+{
+  bool winIsPossible = crossPositions.Length + circlePositions.Length >= 5;
+  if (winIsPossible)
+  {
+    if (IsCrossesTurn)
+    {
+      if (HasWon(circlePositions.ToString()))
+      {
+        RaiseCompletionEvent(Result.CirclesWin);
+        return;
+      }
+    }
+    else
+    {
+      if (HasWon(crossPositions.ToString()))
+      {
+        RaiseCompletionEvent(Result.CrossesWin);
+        return;
+      }
+    }
+
+    bool allMarked = crossPositions.Length + circlePositions.Length == 9;
+    if (allMarked)
+    {
+      RaiseCompletionEvent(Result.Draw);
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+
+private bool HasWon(string positions)
+{
+  return 
+    positions.Contains("012") ||
+    positions.Contains("345") ||
+    positions.Contains("678") ||
+    positions.Contains("036") ||
+    positions.Contains("147") ||
+    positions.Contains("258") ||
+    positions.Contains("048") ||
+    positions.Contains("246");  
+}
+
+//-----------------------------------------------------------------------------
+
+private void ReportMarking(int position)
+{
+  if (IsCrossesTurn)
+  {
+    board.SetMarking(position, Marking.Cross);
+    IsCrossesTurn = false;
+    crossPositions.Append(position);
+    RaiseCrossesMarkEvent(position);
+  }
+  else
+  {
+    board.SetMarking(position, Marking.Circle);
+    IsCrossesTurn = true;
+    circlePositions.Append(position);
+    RaiseCirclesMarkEvent(position);
+  }
+}
+
+//-----------------------------------------------------------------------------
+
+#endregion
+#region Fields
+
+//-----------------------------------------------------------------------------
+
+private Board board = new Board();
+
+//-----------------------------------------------------------------------------
+
+private StringBuilder crossPositions = new StringBuilder();
+private StringBuilder circlePositions = new StringBuilder();
+
+//-----------------------------------------------------------------------------
+
+#endregion
+
+//-----------------------------------------------------------------------------
+
+} // <-- end of type body -->
+
+// ----------------------------------------------------------------------------
+
+} // <-- end of namespace body -->
