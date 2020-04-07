aa19be2d3e537253242119f40a4ad33725435b15
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-04-07 13:32:30 +0200

    App prints welcome message

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,12 +8,19 @@ namespace ConsoleApp {
 
 //-----------------------------------------------------------------------------
 
-class Program {
+public class Program {
 
 //-----------------------------------------------------------------------------
 
 static void Main(string[] args)
 {
+  //Console.WriteLine("Who plays first, Crosses or Circles?\t(Reply with 'X' or 'O')");
+  var messages = new MessagePrinter();
+  messages.PrintWelcome();
+  
+  var reply = Console.ReadLine();
+
+  Console.ReadLine();
 }
 
 //-----------------------------------------------------------------------------
